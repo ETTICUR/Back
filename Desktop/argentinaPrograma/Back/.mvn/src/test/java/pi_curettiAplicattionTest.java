f4982ff657bc4451edad3e0e04e6c98fe3fde484
etticur <ayecuretti@gmail.com>
2022-07-27 12:14:45 -0300

    nuevo intento incial

---
+++ Desktop/argentinaPrograma/Back/.mvn/src/test/java/pi_curettiAplicattionTest.java
@@ -0,0 +1,13 @@
+package com.portfolio.ayecuretti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class pi_curettiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
