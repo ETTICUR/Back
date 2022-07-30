1e0a429fccc2f082700fae409391b5f3e11a9d58
etticur <ayecuretti@gmail.com>
2022-07-30 16:12:55 -0300

    version entregable back

--- Desktop/argentinaPrograma/Back/AyeCuretti/src/main/java/com/portfolio/AyeCuretti/Controller/PersonaController.java
+++ Desktop/argentinaPrograma/Back/AyeCuretti/src/main/java/com/portfolio/AyeCuretti/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portfolio.AyeCuretti.Entity.Persona;
 import com.portfolio.AyeCuretti.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin (origins = "https://localhost:4200")
 /**
  *
  * @author Aye
